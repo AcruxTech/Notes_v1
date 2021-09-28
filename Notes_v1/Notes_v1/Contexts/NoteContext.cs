cfa76d9d6cdd66c09ced4a7febde4fc3b133436d
AcruxTech <acrux_tech@mail.ru>
2021-09-28 19:21:47 +0300

    Обработка Note роутов

---
+++ Notes_v1/Notes_v1/Contexts/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes_v1.Models;
+
+namespace Notes_v1.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext (DbContextOptions<NoteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Note { get; set; }
+    }
+}
